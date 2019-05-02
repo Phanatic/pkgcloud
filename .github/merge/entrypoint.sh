69a450982c6c9ccc6356c03399bb0aacdea84b6a
Phani Raj <phanirajuyn@gmail.com>
2019-05-02 12:37:20 -0500

    actually merge the PR

--- .github/merge/entrypoint.sh
+++ .github/merge/entrypoint.sh
@@ -4,4 +4,4 @@ PULL_URL=$(cat "$GITHUB_EVENT_PATH" | jq -r '.pull_request.url')
 
 set +x
 
-curl -Ss -H "Authorization: token $GITHUB_TOKEN" "$PULL_URL/merge" > /dev/null
+curl -Ss -XPUT -H "Authorization: token $GITHUB_TOKEN" "$PULL_URL/merge" > /dev/null
